72280c437e6f329a548b6c0071071e124e2752e4
Sumita5 <satyakiroyhreeg@gmail.com>
2020-08-08 09:54:39 +0600

    learn advanced javascript

---
+++ array-splice.js
@@ -0,0 +1,10 @@
+const nums = [1,2,3,4,5,6,7,8];
+// const part = nums.slice(3,6);
+
+const removed = nums.splice(2,3,77,90);
+// console.log(removed);
+// // console.log(part);
+// console.log(nums);
+
+const together = nums.join(",");
+console.log(together);
--- scope.js
+++ scope.js
@@ -1,6 +1,20 @@
+let bonus = 20;
 function sum(first, second){
-    let result = first + second;
-    console.log(result);
+    let result = first + second + bonus;
+    // console.log(bonus);
+    if(result > 9){
+        var mood = "happy";
+        mood = "fishy";
+        mood = "silly";
+        // console.log(mood);
+    }
+
+    console.log(day);
+    let day = "friday";
     return result;
 }
-const output = sum[3, 7];
+const output = sum(3, 7);
+// console.log(output);
+// console.log(bonus);
+// we learn this lesson scope,whisting,let,const and var
+//  and learn a article from thatjsdude.com
